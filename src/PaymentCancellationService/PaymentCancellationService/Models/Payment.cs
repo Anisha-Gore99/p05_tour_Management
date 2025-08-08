86e5a6f9a812d754f97fdb86507d151227f9391e
siddhikarande09 <siddhi.karande20@pccoepune.org>
2025-08-08 21:11:25 +0530

    Created payment and cancellation service in dot net

---
+++ src/PaymentCancellationService/PaymentCancellationService/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentCancellationService.Models;
+
+public partial class Payment
+{
+    public int PaymentId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateOnly PaymentDate { get; set; }
+
+    public int? ModeId { get; set; }
+
+    public int? BookingId { get; set; }
+
+    public virtual Booking? Booking { get; set; }
+
+    public virtual PaymentMode? Mode { get; set; }
+
+    public virtual ICollection<Refund> Refunds { get; set; } = new List<Refund>();
+}
---
+++ src/PaymentCancellationService/PaymentCancellationService/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentCancellationService.Models;
+
+public partial class User
+{
+    public int Uid { get; set; }
+
+    public string? Uname { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public string? PhoneNo { get; set; }
+
+    public string? Email { get; set; }
+
+    public int Rid { get; set; }
+
+    public virtual Role RidNavigation { get; set; } = null!;
+
+    public virtual TourAgency? TourAgency { get; set; }
+
+    public virtual ICollection<Tourist> Tourists { get; set; } = new List<Tourist>();
+}
