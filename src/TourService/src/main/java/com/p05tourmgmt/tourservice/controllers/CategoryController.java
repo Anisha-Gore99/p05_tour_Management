7ba3e991b5a74a7fb601bc06eba49b1dce6024c2
Shreya jadhav <jadhavshreya297@gmail.com>
2025-08-08 21:21:21 +0530

    changes

--- src/TourService/src/main/java/com/p05tourmgmt/tourservice/controllers/CategoryController.java
+++ src/TourService/src/main/java/com/p05tourmgmt/tourservice/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.p05tourmgmt.tourservice.entities.Category;
 import com.p05tourmgmt.tourservice.services.CategoryService;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
