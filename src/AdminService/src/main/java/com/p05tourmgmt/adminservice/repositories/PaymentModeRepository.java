9ac6220338933f84ca9cd010b246db51b21f0d3e
Anisha-Gore99 <anishagoress9204@gmail.com>
2025-08-17 21:09:03 +0530

    deleted AdminService

--- src/AdminService/src/main/java/com/p05tourmgmt/adminservice/repositories/PaymentModeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.p05tourmgmt.adminservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.p05tourmgmt.adminservice.entities.PaymentMode;
-
-public interface PaymentModeRepository extends JpaRepository<PaymentMode, Integer> {
-
-}
